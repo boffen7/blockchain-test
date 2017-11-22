9c1676fbb405b382227d74bf13b593a3558a6d0b
boffen7 <lolklokke@gmail.com>
2017-11-22 19:49:39 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
-const SHA256 = require(crypto-js/SHA256)
+const SHA256 = require('crypto-js/sha256')
 
-class block{
+class Block{
     constructor(index, timestamp, data, previousHash = ''){
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+
+        }
+        
+        console.log("Block mined: " + this.hash);
     }
 }
+
+
 
 class Blockchain {
     constructor(){
         this.chain = [this.createGenesisBlock];
+        this.difficulty = 4;
 
     }
 
@@ -30,13 +44,33 @@ class Blockchain {
 
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
+    }
+
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
 
+
 let boffenCoin = new Blockchain();
+
+console.log('mining block 1...')
 boffenCoin.addBlock(new Block(1, "10/11/2017", {amount: 4 }));
+console.log('mining block 2...')
 boffenCoin.addBlock(new Block(2, "12/11/2017", {amount: 10 }));
 
-console.log(JSON.stringify(boffenCoin, null, 4));
